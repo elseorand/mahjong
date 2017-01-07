15172c006ada757e4cd8a1a3dfefa82911f9f701
elseorand <sumita@fg8.so-net.ne.jp>
2017-01-07 16:37:52 +0900

    add websocket init

--- src/main/scala/com/elseorand/game/mahjong/server/WebServer.scala
+++ src/main/scala/com/elseorand/game/mahjong/server/WebServer.scala
@@ -13,6 +13,7 @@ import akka.http.scaladsl._
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.ws._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.util._
 
@@ -29,36 +30,43 @@ object WebServer extends App {
   val route =
     get {
       pathPrefix("mahjong") {
-        pathEndOrSingleSlash {
-          getFromResource("html" / "main.html")
+        pathSingleSlash {
+          getFromResource("html/main.html")
         } ~
         pathPrefix("jscss"){
           getFromResourceDirectory("jscss") // TODO search recursive
         } ~
         pathPrefix("jscss" / "jquery-ui-1.11.4") {
           getFromResourceDirectory("jscss/jquery-ui-1.11.4")
+        } ~
+        pathPrefix("ws") {
+          // TODO 誰が捨てたかが、必須のため、受領を意味するHttpレスポンスも返す
+          handleWebSocketMessages(wsFlow)// TODO wsFlowの生成にHttpRequestの情報を用いる
         }
-      } ~
-      path("sutehai") {
-        // TODO 誰が捨てたかが、必須のため、受領を意味するHttpレスポンスも返す
-
-        handleWebSocketMessages(sutehaiFlow)// TODO sutehaiFlowの生成にHttpRequestの情報を用いる
       }
     }
 
   val broadCaster = system.actorOf(Props[BroadcastActor])
 
-  def sutehaiFlow() = Flow.fromGraph(GraphDSL.create(Source.actorRef[Sutehai](???)) {
-    implicit builder =>
-    subscribeActor =>
-    import GraphDSL.Implicits._
+  // 下記 `handleWebSocketMessages` で使用される Flow (Strict 版) // TODO back pressure やるのでその内修正
+  val wsFlow: Flow[Message, Message, _] =
+      Flow[Message].map {
+        case TextMessage.Strict(txt) => {
+          // 完全なメッセージを受信するまで待ちます。
+          // Strict (完全なメッセージ) を受信してから、完全なメッセージで応答します。
+          // つまり、TextMessage(textStream: Source[String, Any]) ではなく TextMessage(text: String) で応答します。
+          TextMessage("Hello " + txt)
+        }
+        case _ => {
+          // バイナリメッセージを受信
+          TextMessage("Message type unsupported")
+        }
+      }
 
-    val websocketSource = builder.add(Flow[Message].collect (???))
-
-  })
 
   val bindingFuture = Http().bindAndHandle(route, "localhost", serverPort)
 
+  // EnterでSeverを終了
   println(s"Server online at http://localhost:$serverPort\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
   bindingFuture
