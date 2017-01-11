b69283e16d1ac7364b12b1efd410524c1175ea20
elseorand <sumita@fg8.so-net.ne.jp>
2017-01-11 09:25:20 +0900

    mod: WS onmessage works.

--- src/main/resources/jscss/app.module.js
+++ src/main/resources/jscss/app.module.js
@@ -1,21 +1,55 @@
 $(function(){
-  var webConn = new WebSocket('ws://localhost:18080/mahjong/ws');
-  webConn.onopen = function(event) {
-    console.log('onopen');
-  };
+  var href = window.location.href ;
+  var rawProtocol = window.location.protocol;
+  var protocol = rawProtocol.substr( 0 , (rawProtocol.length-1) ) ;
+  var hostname = window.location.hostname ;
+  Object.keys(window.location).forEach(function(x,i,a){
 
-  webConn.onclose = function(event) {
-    console.log('onclose');
-  };
+  });;
 
-  webConn.onmessage = function(event) {
-    if (event && event.datta){
-      console.log('onmessage: ' + event.data);
-    }
-  };
+  console.log('hostname : ' + JSON.stringify(hostname));
 
-  webConn.onerror = function(event) {
-    console.log('onerror');
-  };
+  var webConn = null;
+  function init(){
+    webConn = new WebSocket('ws://' + hostname + ':18080/mahjong/ws');
+    webConn.onopen = function(event) {
+      console.log('onopen: ' + event.data);
+    };
+
+    webConn.onclose = function(event) {
+      console.log('onclose');
+    };
+
+    webConn.onmessage = function(event) {
+      if (event && event.data){
+        console.log('event: ' + JSON.stringify(event));
+        console.log('onmessage: ' + event.data);
+      } else {
+        console.log('event.prototype : ' + JSON.stringify(event.prototype));
+        console.log('arguments.length : ' + JSON.stringify(arguments.length));
+        console.log('event: ' + JSON.stringify(event));
+        console.log('happen:');
+      }
+    };
+
+    webConn.onerror = function(event) {
+      console.log('onerror');
+      init();
+    };
+  }
+  init();
+
+  var el = {};
+  var els_element = $('.element').each(function(){
+    var _this = $(this);
+    el[_this.attr('id')] = _this;
+  });
+
+  el.test.on('click', function(){
+    var _this = $(this);
+    console.log('click : ');
+    webConn.send('test');
+  });
+
 
 });
--- src/main/scala/com/elseorand/game/mahjong/server/WebServer.scala
+++ src/main/scala/com/elseorand/game/mahjong/server/WebServer.scala
@@ -1,25 +1,16 @@
-import scala.concurrent._
 import scala.io.StdIn
 
 import collection.mutable
-import collection.JavaConversions._
 
-import akka._
 import akka.actor._
 import akka.stream._
 import akka.stream.scaladsl._
-import akka.http._
 import akka.http.scaladsl._
-import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.ws._
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.util._
-
-import spray.json.DefaultJsonProtocol._
 
 object WebServer extends App {
+  // args
   implicit val system = ActorSystem("mahjong")
   implicit val materializer = ActorMaterializer()
   // needed for the future flatMap/onComplete in the end
@@ -27,44 +18,36 @@ object WebServer extends App {
 
   implicit val serverPort = 18080
 
-  val route =
-    get {
-      pathPrefix("mahjong") {
-        pathSingleSlash {
-          getFromResource("html/main.html")
-        } ~
-        pathPrefix("jscss"){
-          getFromResourceDirectory("jscss") // TODO search recursive
-        } ~
-        pathPrefix("jscss" / "jquery-ui-1.11.4") {
-          getFromResourceDirectory("jscss/jquery-ui-1.11.4")
-        } ~
-        pathPrefix("ws") {
-          // TODO 誰が捨てたかが、必須のため、受領を意味するHttpレスポンスも返す
-          handleWebSocketMessages(wsFlow)// TODO wsFlowの生成にHttpRequestの情報を用いる
-        }
+  val route = get {
+    pathPrefix("mahjong") {
+      pathSingleSlash {
+        getFromResource("html/main.html")
+      } ~
+      pathPrefix("jscss"){
+        getFromResourceDirectory("jscss") // TODO search recursive
+      } ~
+      pathPrefix("jscss" / "jquery-ui-1.11.4") {
+        getFromResourceDirectory("jscss/jquery-ui-1.11.4")
       }
     }
-
-  val broadCaster = system.actorOf(Props[BroadcastActor])
+  } ~
+  pathPrefix("mahjong" / "ws") {
+    Console println "pass : "
+    // TODO 誰が捨てたかが、必須のため、受領を意味するHttpレスポンスも返す
+    handleWebSocketMessages(wsFlow)// TODO wsFlowの生成にHttpRequestの情報を用いる
+  }
 
   // 下記 `handleWebSocketMessages` で使用される Flow (Strict 版) // TODO back pressure やるのでその内修正
-  val wsFlow: Flow[Message, Message, _] =
-      Flow[Message].map {
-        case TextMessage.Strict(txt) => {
-          // 完全なメッセージを受信するまで待ちます。
-          // Strict (完全なメッセージ) を受信してから、完全なメッセージで応答します。
-          // つまり、TextMessage(textStream: Source[String, Any]) ではなく TextMessage(text: String) で応答します。
-          TextMessage("Hello " + txt)
-        }
-        case _ => {
-          // バイナリメッセージを受信
-          TextMessage("Message type unsupported")
-        }
-      }
+  val wsFlow: Flow[Message, Message, _] = Flow[Message].mapConcat {
+    case tm: TextMessage =>
+      TextMessage(Source.single("Hello ") ++ tm.textStream ++ Source.single("!")) :: Nil
+    case bm: BinaryMessage =>
+      // ignore binary messages but drain content to avoid the stream being clogged
+      bm.dataStream.runWith(Sink.ignore)
+      Nil
+  }
 
-
-  val bindingFuture = Http().bindAndHandle(route, "localhost", serverPort)
+  val bindingFuture = Http().bindAndHandle(route, "192.168.11.3", serverPort)
 
   // EnterでSeverを終了
   println(s"Server online at http://localhost:$serverPort\nPress RETURN to stop...")
